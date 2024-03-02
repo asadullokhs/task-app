178a70ba1492f403d9a6cf34143016c8571d28b5
Asadullokh <uzbdevit@gmail.com>
2024-03-02 19:26:42 +0500

    i have added sending to email

--- src/controller/mainCtrl.js
+++ src/controller/mainCtrl.js
@@ -31,6 +31,44 @@ const main = {
 
     res.render("404", { locals });
   },
+  sendMail: async (req, res) => {
+    const { name, company, email, phone, message } = req.body;
+    try {
+      const config = {
+        service: "gmail",
+        auth: {
+          user: "uzbdevit@gmail.com",
+          pass: process.env.PASSWORD,
+        },
+      };
+
+      let transporter = nodemailer.createTransport(config);
+
+      const ouput = `
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${name}</li>
+        <li>Company: ${company}</li>
+        <li>Email: ${email}</li>
+        <li>Phone: ${phone}</li>
+        <li>Message: ${message}</li>
+      </ul>
+      `;
+
+      const msg = {
+        to: ["muhammadbilol1011@gmail.com", "faxrillo67@gmail.com"],
+        from: "uzbdevit@gmail.com",
+        subject: "Contact Request",
+        text: "Mail is sent by Sendgrid App",
+        html: ouput,
+      };
+
+      transporter.sendMail(msg);
+      res.render("status");
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = main;
--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -4,5 +4,6 @@ const mainCtrl = require("../controller/mainCtrl");
 
 router.get("/", mainCtrl.home);
 router.get("/about", mainCtrl.about);
+router.get("/contact", mainCtrl.contact);
 
 module.exports = router;
